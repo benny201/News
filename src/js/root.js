e011aed068f48d018764551264d8dca992d48921
447093943@qq.com <zwu58@ucsc.edu>
2017-02-21 14:03:54 -0800

    update

--- src/js/root.js
+++ src/js/root.js
@@ -1,25 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Index from './index';
-import ComponentList from './components/componentList'
-import Detail from './components/details'
 
 import {Router, Route, hashHistory} from 'react-router';
 
-export default class Root extends React.Component {
-    render() {
-        return (
-            <Router history={hashHistory}>
-                <Route component={Index} path="/">
-                    <Route component={Detail} path="details"></Route>
-                </Route>
-                <Route component={ComponentList} path="list/:id"></Route>
-            </Router>
-        )
-
-
-    };
-}
+// export default class Root extends React.Component {
+//     render() {
+//         return (
+//
+//         )
+//
+//
+//     };
+// }
 
 
 ReactDOM.render(
