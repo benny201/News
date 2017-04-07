51c674351d79a601404be01f36ae5c68a0152dad
447093943@qq.com <zwu58@ucsc.edu>
2017-04-07 16:15:04 -0700

    update

--- src/js/components/pc_index.js
+++ src/js/components/pc_index.js
@@ -8,7 +8,7 @@ export default class PCIndex extends React.Component {
     render() {
         return (
             <div>
-                {/*<PCHeader/>*/}
+                <PCHeader/>
                 <PCContainer/>
                 <PCFooter/>
             </div>
